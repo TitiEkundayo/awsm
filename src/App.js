b0cacdb6181d4f19d332b97fdf1a92481218de54
profgreatwonder <muoghaluchidinma@gmail.com>
2022-09-02 00:09:15 +0100

    renamed all page files using the pascal case convention to the sentence case convention

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GuestTopMenu, MenuNavbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/Homepage";
-import BookPage from "./pages/BookPage";
-import CommunityPage from "./pages/CommunityPage";
-import DashboardPage from "./pages/DashboardPage";
-import PodcastPage from "./pages/PodcastPage";
+import BookPage from "./pages/Bookpage";
+import CommunityPage from "./pages/Communitypage";
+import DashboardPage from "./pages/Dashboardpage";
+import PodcastPage from "./pages/Podcastpage";
 
 function App() {
 	return (
