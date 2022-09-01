f9de696c947a554de2311ae0b6d97613f2176701
profgreatwonder <muoghaluchidinma@gmail.com>
2022-09-01 22:58:26 +0100

    made corrections to the component name HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { GuestTopMenu, MenuNavbar } from "./components/Navbar";
-import Homepage from "./pages/HomePage";
-import BookPage from "./pages/BookPage";
-import Footer from "./components/Footer";
-import PodcastPage from "./pages/PodcastPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GuestTopMenu, MenuNavbar } from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import BookPage from "./pages/BookPage";
 import CommunityPage from "./pages/CommunityPage";
 import DashboardPage from "./pages/DashboardPage";
+import PodcastPage from "./pages/PodcastPage";
 
 function App() {
 	return (
@@ -13,7 +13,7 @@ function App() {
 			{/* <GuestTopMenu />
 			<MenuNavbar /> */}
 			<Routes>
-				<Route path="/" element={<Homepage />} />
+				<Route path="/" element={<HomePage />} />
 				<Route path="bookpage" element={<BookPage />} />
 				<Route path="podcastpage" element={<PodcastPage />} />
 				<Route path="communitypage" element={<CommunityPage />} />
